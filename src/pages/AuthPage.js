178d8a3c2002299d268a6bbda28c83bb1fbce90d
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-10-05 14:54:34 +0530

    sign up page added

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { jobAction } from "../store/jobsData";
 import { useNavigate } from "react-router-dom";
+
 function AuthPage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [signupState, setSignupState] = useState(false);
+  const [signinState, setsignInState] = useState(true);
   function submitHandler(e) {
     e.preventDefault();
     dispatch(jobAction.authentication(true));
@@ -35,6 +38,16 @@ function AuthPage() {
                 placeholder="Enter email id"
                 required=""
               />
+              {signupState && (
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className=" mt-3 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Confirm email id"
+                  required=""
+                />
+              )}
             </div>
             <div>
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -49,49 +62,44 @@ function AuthPage() {
                 required=""
               />
             </div>
-            <div className="flex items-center justify-between">
-              <div className="flex items-start">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    aria-describedby="remember"
-                    type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    required=""
-                  />
-                </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    for="remember"
-                    className="text-gray-500 dark:text-gray-300"
-                  >
-                    Remember me
-                  </label>
-                </div>
-              </div>
-              <a
-                href="#"
-                className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-              >
-                Forgot password?
-              </a>
-            </div>
+            <div className="flex items-center justify-between"></div>
             <button
               type="submit"
               className="w-full text-white
               border-2 bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
-              Sign in
+              {signinState ? "Sign in" : "Sign up"}
             </button>
-            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-              Don’t have an account yet?{" "}
-              <a
-                href="#"
-                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-              >
-                Sign up
-              </a>
-            </p>
+            {signinState ? (
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?{" "}
+                <a
+                  onClick={() => {
+                    setSignupState(true);
+                    setsignInState(false);
+                  }}
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign up
+                </a>
+              </p>
+            ) : (
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Click on sign in to sign in?{" "}
+                <a
+                  onClick={() => {
+                    setsignInState(true);
+                    setSignupState(false);
+                    console.log(signinState);
+                  }}
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Sign in
+                </a>
+              </p>
+            )}
           </form>
         </div>
       </div>
